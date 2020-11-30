5aebbca1728701ee668a161cbfb1e112b18e9901
Chen Sun <chensun@users.noreply.github.com>
2020-11-30 13:40:52 -0800

    update init-python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,11 +1,20 @@
 ;;; init-python.el
 (require-package 'elpy)
 (require-package 'jedi)
+(require-package 'ein)
 
 (setq elpy-rpc-python-command "python3")
 (setq elpy-rpc-virtualenv-path 'current)
 
 (elpy-enable)
+
+(defun chesu-python-mode-hook ()
+  (display-fill-column-indicator-mode)
+  (setq fill-column 80)
+  (setq python-indent-offset 2))
+
+(add-hook 'python-mode-hook
+          'chesu-python-mode-hook)
 
 (provide 'init-python)
 ;;; init-python.el ends here
